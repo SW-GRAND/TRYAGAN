5289df85d664897a331457437c5894df3c0076b4
SW-GRAND <stokolosav@mail.ru>
2016-02-26 13:49:53 +0200

    Коментар 1

---
+++ Slaydy/Slaydy/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Slaydy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Slaydy.Properties.Resources._8i__;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(311, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 96);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::Slaydy.Properties.Resources.іііів;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Location = new System.Drawing.Point(654, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 143);
+            this.button2.TabIndex = 3;
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(240, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Час і Дата";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "D:\\";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(568, 329);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::Slaydy.Properties.Resources._1221;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button3.Location = new System.Drawing.Point(663, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 120);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImage = global::Slaydy.Properties.Resources.b;
+            this.button4.Location = new System.Drawing.Point(435, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 80);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImage = global::Slaydy.Properties.Resources.g;
+            this.button5.Location = new System.Drawing.Point(218, 409);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 80);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Hobo Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(12, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 27);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(847, 527);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Slayder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Slaydy/Slaydy/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Slaydy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.timer1.Interval = 1000;
+            this.timer1.Enabled = true;
+            this.timer1.Stop();
+            this.folderBrowserDialog1.Description = "Виберіть, будь-ласка, каталог з файлами типу .jpg (фотографії).";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            this.folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            DirectoryInfo d = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            int i;
+            i = Convert.ToInt16(label2.Text);
+            FileInfo[] fis = d.GetFiles("*.jpg");
+            if (fis.GetLength(0) == 0)
+            {
+                MessageBox.Show("Виберіть, будь-ласка, інший каталог. У цьому немає .jpg-файлів");
+                return;
+            }
+            if (i > fis.GetLength(0))
+            {
+                i = 0;
+                label2.Text = i.ToString();
+            }
+            pictureBox1.ImageLocation = fis[1].DirectoryName + "\\" + fis[i].Name;
+            pictureBox1.Load();
+            button2.Visible = true;
+            button5.Visible = true;
+            button4.Visible = true;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.button2.Text == "Старт")
+            {
+                this.button2.Text = "Стоп";
+                this.timer1.Start();
+                //button2.Visible = false;
+            }
+            else
+            {
+                this.button2.Text = "Старт";
+                this.timer1.Stop();
+                // button2.Visible = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = Convert.ToString(System.DateTime.Now);
+            int i;
+            i = Convert.ToUInt16(label2.Text);
+            i++;
+            label2.Text = i.ToString();
+            DirectoryInfo d = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] fis = d.GetFiles("*.jpg");
+            if (i >= fis.GetLength(0))
+            {
+                i = 0; label2.Text = i.ToString();
+            }
+            {
+                pictureBox1.ImageLocation = fis[i].DirectoryName + "\\" + fis[i].Name;
+                pictureBox1.Load();
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = Convert.ToString(System.DateTime.Now);
+            int i;
+            i = Convert.ToUInt16(label2.Text);
+            i++;
+            label2.Text = i.ToString();
+            DirectoryInfo d = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] fis = d.GetFiles("*.jpg");
+            if (i >= fis.GetLength(0))
+            {
+                i = 0; label2.Text = i.ToString();
+            }
+            {
+                pictureBox1.ImageLocation = fis[i].DirectoryName + "\\" + fis[i].Name;
+                pictureBox1.Load();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = Convert.ToString(System.DateTime.Now);
+            int i;
+            i = Convert.ToUInt16(label2.Text);
+            i--;
+            label2.Text = i.ToString();
+            DirectoryInfo d = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] fis = d.GetFiles("*.jpg");
+            if (i >= fis.GetLength(0))
+            {
+                i = 0; label2.Text = i.ToString();
+            }
+            {
+                pictureBox1.ImageLocation = fis[i].DirectoryName + "\\" + fis[i].Name;
+                pictureBox1.Load();
+                
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+ 
+
+    
